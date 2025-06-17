c211312e4ec5b9745813920ddb755dc32bb27c2f
GossiperLoturot <ts20030211@gmail.com>
2025-06-17 09:37:55 +0900

    fix for windows

--- Plugin/src/dynamic_mesh.cpp
+++ Plugin/src/dynamic_mesh.cpp
@@ -3,7 +3,7 @@
 
 int add(int a, int b) { return a + b; }
 
-extern "C" UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API int add_unity(int a,
+extern "C" int UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API add_unity(int a,
                                                                     int b) {
   return add(a, b);
 }
